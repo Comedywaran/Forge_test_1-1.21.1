3fdd68ac71d580487115e597dd77d4c13463c329
Timo <m1n3cr4f7undr0bl0x@gmail.com>
2026-02-01 20:08:24 +0100

    deleted example item and block & made uran ingot edible

--- src/main/java/de/timo_heise/timos_test_mod/item/ModItems.java
+++ src/main/java/de/timo_heise/timos_test_mod/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.timo_heise.timos_test_mod.item;
 
 import de.timo_heise.timos_test_mod.TestMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
 
-    public static final RegistryObject<Item> URAN_INGOT = ITEMS.register("uran_ingot", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> URAN_INGOT = ITEMS.register("uran_ingot", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(2048).saturationModifier(4096f).build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
